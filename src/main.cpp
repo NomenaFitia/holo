40395428ad6165cf5ad8ff3eeb4a7aaa80c954bf
Fitia <nomenaandriamahafaly@gmail.com>
2025-05-27 16:16:14 +0200

    update Cmakelists - vcpkg.json

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@
 #include <DirectXRenderer.h>
 
 
-int main() {
+int main() 
+{
 	int width, height, depth;
     std::string directoryPath = "C:\\Users\\MIPA\\Documents\\Projets\\dcm";
 	float isovalue = 0.5f;
@@ -15,24 +16,8 @@ int main() {
 		DICOMLoader loader;
 		VolumeData volume = loader.loadFromDirectory(directoryPath);
 
-		/*
-		* 
-		* 
-		std::cout << "Loaded volume with dimensions: " << volume.width << " x " << volume.height << " x " << volume.depth << std::endl;
-
-		for (size_t i = 0; i < volume.width * volume.height * volume.depth; i++)
-		{ 
-			if (volume.voxels[i] > 65535) {
-				std::cout << "Loaded :" << volume.voxels[i] << std::endl;
-			}
-		}
-
-		*/
-
 		auto triangles = MarchingCubes::generateSurface(volume, 30000); // confirmation de la valeur d'iso pour le squlette 
-
-		// Sauvegarder en OBJ
-		MarchingCubes::saveToObj(triangles, "output.obj");
+		//MarchingCubes::saveToObj(triangles, "output.obj");
 
 	}
 	catch (const std::exception& e) {
